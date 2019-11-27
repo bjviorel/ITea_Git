56f2113a40ca9b208167ab6e6af65c34c5265607
bjviorel <bjviorel@yahoo.com>
2019-11-27 13:56:45 +0200

    sout 2

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,12 +1,14 @@
 public class HelloWorld {
 
-    public static void main(String[] args){
+public static void main(String[] args){
         System.out.println("Added the Git folder");
         System.out.println("Added the Project in maven");
         System.out.println("I did first commit now I'm doing the second commit");
         System.out.println("I did other commits also now");
-    }
+    System.out.println(1);
+    System.out.println(2);
+        }
 
 
 
-}
+        }
