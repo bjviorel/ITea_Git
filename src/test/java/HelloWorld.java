4319d033374c9c4cfcf35a3e35133f47a07b1056
bjviorel <bjviorel@yahoo.com>
2019-11-27 13:36:37 +0200

    new data for second branch

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         System.out.println("Added the Project in maven");
         System.out.println("I did first commit now I'm doing the second commit");
         System.out.println("I did other commits also now");
+        System.out.println("I have created a new branch. Now I want to check how to push the data on the second branch");
     }
 
 
