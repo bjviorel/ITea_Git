2d9aa4cd15bb8dfeec7855b636da952aa2e52e84
bjviorel <bjviorel@yahoo.com>
2019-11-27 14:47:22 +0200

    example from Mihaela

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,13 +1,7 @@
 public class HelloWorld {
 
 public static void main(String[] args){
-        System.out.println("Added the Git folder");
-        System.out.println("Added the Project in maven");
-        System.out.println("I did first commit now I'm doing the second commit");
-        System.out.println("I did other commits also now");
-    System.out.println(1);
-    System.out.println(2);
-    System.out.println(3);
+    System.out.println("Cana, its an example from Mihaela");
         }
 
 
