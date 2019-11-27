317b8e022d82c3222051e3a8281c18acfcb6ed3f
bjviorel <bjviorel@yahoo.com>
2019-11-27 13:02:34 +0200

    third commit

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Added the Git folder");
         System.out.println("Added the Project in maven");
+        System.out.println("I did first commit now I'm doing the second commit");
     }
 
 
