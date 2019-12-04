5b9708f13516271df98c48c0210a7693e802f450
bjviorel <bjviorel@yahoo.com>
2019-12-04 09:10:45 +0200

    new commit

--- src/test/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Test 1 for Git");
-        System.out.println("Test 2 for Git");
-        System.out.println("Test 3 for Git");
-        System.out.println("Test 4 for Git");
-    }
-}
--- src/test/java/ITeaHome.java
+++ src/test/java/ITeaHome.java
@@ -2,6 +2,5 @@ public class ITeaHome {
 
     public static void main(String[] args) {
         System.out.println("Welcome to ITea Home Academy");
-        System.out.println("Viorel, this is a simple java program, try to do your best to exercise ");
     }
 }
